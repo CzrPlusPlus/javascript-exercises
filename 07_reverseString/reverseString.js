91a0f2880452638c5551139f757972626ceef38c
Ceasar <ceasarmoya14@gmail.com>
2025-11-06 19:48:06 -0800

    Exercise 7 complete

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = ""
+    for (let i = string.length-1; i >= 0; i--){
+        result += string[i];
+    }
+    return result;
 };
+
+let myString = "hello";
+let reversedString = reverseString(myString);
+console.log(reversedString);
 
 // Do not edit below this line
 module.exports = reverseString;
