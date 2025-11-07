6801b9c60747dc80a8e8bb4cf0b50e634d8b51d9
Ceasar <ceasarmoya14@gmail.com>
2025-11-06 20:37:48 -0800

    Exercise 8 complete

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let result = [];
+    let duplicate;
+    for (let i = 0; i < array.length; i++){
+        duplicate = false;
+        for (let j = 0; j < args.length; j++){
+            if (array[i] === args[j]){
+                duplicate = true;
+                break;
+            }
+        }
+        if (!duplicate){
+            result.push(array[i])
+        }
+    }
+    return result;
 };
 
+let myArray = [1, 2, 3, 4, 5];
+let newArray = removeFromArray(myArray, 3, 4, 5, 6, 1)
+console.log(newArray);
 // Do not edit below this line
 module.exports = removeFromArray;
