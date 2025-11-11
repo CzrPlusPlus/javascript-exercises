c69189e50a774dd05ffb7d9d8422627f5df8c2cd
Ceasar <ceasarmoya14@gmail.com>
2025-11-10 18:09:50 -0800

    Exercise 10 complete

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // the year must be divisible by 4
+    // years divisible by 100 are not leap years UNLESS they are divisible by 400
+    if((year % 4 == 0 && year % 100 != 0) || (year % 4 == 0 && year % 400 == 0)){
+        return true;
+    }
+    return false;
 };
+
+// console.log(leapYears(2000));
+// console.log(leapYears(1996));
+// console.log(leapYears(1995));
+// console.log(leapYears(1900));
+
+
 
 // Do not edit below this line
 module.exports = leapYears;
