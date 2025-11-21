b51867ace163cd99428816e6b4a5be2fc4fe1d2e
Ceasar <ceasarmoya14@gmail.com>
2025-11-20 19:10:47 -0800

    exercise 12 complete

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, number) => acc + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, number) => acc * number, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  for (let i = 1; i <= num; i++){
+    result *= i;
+  }
+  return result;
 };
+
+// let a = [1, 2, 3, 4, 5];
+// let b = [10, 20, 30];
+// console.log(sum(a));
+// console.log(sum(b));
+// console.log(multiply(a));
+// console.log(multiply(b));
+// console.log(power(2, 6));
+// console.log(factorial(5));
+
 
 // Do not edit below this line
 module.exports = {
