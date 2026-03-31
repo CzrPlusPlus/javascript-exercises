1767cdf0b332c0216f716aac638c2d0d6354f3b2
Ceasar <ceasarmoya14@gmail.com>
2026-03-31 15:04:08 -0700

    Complete foundation exercise 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+  // given the array arr, remove all arguments from the given array
+  let resultArray = [];
+  arr.forEach((element) => {
+    if (!theArgs.includes(element)){
+      resultArray.push(element);
+    }
+  })
+  return resultArray;
 };
 
 // Do not edit below this line
