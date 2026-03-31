b45a329fbc471311ef990e6f9a74cc905af77e29
Ceasar <ceasarmoya14@gmail.com>
2026-03-31 15:18:20 -0700

    Complete foundation exercise 09
    Alternative approach would be to name the parameters min & max and swap
    the values if min is greater than max. In this case, don't need to write
    two for loops.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)){
+    return "ERROR";
+  }
+  let accumulator = 0;
+  if (num1 < num2){
+    for (let i = num1; i <= num2; i++){
+      accumulator += i;
+    }
+  }
+  else {
+    for (let i = num2; i <= num1; i++){
+      accumulator += i;
+    }
+  }
+  return accumulator;
 };
 
 // Do not edit below this line
