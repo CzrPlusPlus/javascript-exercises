e0e9ca7e4a1b3681ac56e551f9456a97f63a2dfa
Ceasar <ceasarmoya14@gmail.com>
2026-03-31 17:05:10 -0700

    Complete foundation exercise 10

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  // leap years are divisible by 4
+  // however, years divisible by 100 are NOT leap years unless they are divisible by 400
+  if (year % 4 == 0){
+    if (year % 400 == 0){
+      return true;
+    }
+    else if (year % 100 == 0){
+      return false;
+    }
+    return true;
+  }
+  else {
+    return false;
+  }
 };
 
 // Do not edit below this line
