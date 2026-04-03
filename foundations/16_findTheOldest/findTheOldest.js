eae17c2cbd15fd19aa663686e9f485e506e74014
Ceasar <ceasarmoya14@gmail.com>
2026-04-02 18:28:55 -0700

    Complete foundation exercise 16
    My approach was to sort the array by age and return the first element.
    Creating a helper function to calculate the age helped significantly.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
+function getAge(person){
+  person.yearOfDeath ??= new Date().getFullYear();
+  return person.yearOfDeath - person.yearOfBirth;
+}
 
+const findTheOldest = function(arr) {
+  let sortedArr = arr.sort((a, b) => getAge(a) > getAge(b) ? -1 : 1);
+  return sortedArr[0];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
